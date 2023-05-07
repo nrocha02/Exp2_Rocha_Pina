b2c16fc84db5eebf06eff33e7aeca91424334022
Nicolás Rocha <thewolf2732@gmail.com>
2023-05-07 19:26:00 -0400

    Implemetacion api

---
+++ js/api.js
@@ -0,0 +1,20 @@
+let url='https://api.thedogapi.com/v1/breeds?limit=10&page=0';
+fetch(url)
+	.then(response =>response.json())
+	.then(data => mostrarData(data))
+	.catch(error => console.log(error))
+
+const mostrarData=(data)=>{
+	console.log(data)
+	let body=""
+	for (var i=0; i<data.length; i++)
+	{
+		body+=`<tr>                      
+			<td>${data[i].id}</td>       
+			<td>${data[i].name}</td>
+			<td>${data[i].life_span}</td>
+			<td><img src="${data[i].image.url}" alt="${data[i].name}"></td>
+			</tr>`
+	}
+	document.getElementById('data').innerHTML=body
+}
